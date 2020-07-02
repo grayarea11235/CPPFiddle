92e1928bbdb4f67696a1e50502d6a91d1f6b4d40
grayarea11235 <ciaran.dunn@gmail.com>
2020-07-02 14:36:23 +0100

    Made to work on other machine... there is soemthign wrong with the lib on main machine

--- Poco/poco_json_ex.cpp
+++ Poco/poco_json_ex.cpp
@@ -31,33 +31,42 @@ string GetValue(Object::Ptr aoJsonObject, const char *aszKey)
 int main(void)
 {
   Parser loParser;
-  std::string lsJson = "{\"TransactionCo	de\":\"000000\",\"FileRecordSequenceNumber\":\"111111\",\"TcrSequenceNumber\":\"222222\",\"TransactionRouteIndicator\":\"ABCDE\",\"MerchantEstablishmentNumber\":\"00000onStop000000\",\"MerchantName\":\"BBBBBBBBB\",\"MerchantCity\":\"CCCCCCCC\"}";
+  std::string lsJson = "{\"TransactionCode\":\"000000\",\"FileRecordSequenceNumber\":\"111111\",\"TcrSequenceNumber\":\"222222\",\"TransactionRouteIndicator\":\"ABCDE\",\"MerchantEstablishmentNumber\":\"00000onStop000000\",\"MerchantName\":\"BBBBBBBBB\",\"MerchantCity\":\"CCCCCCCC\"}";
 
-  std::cout << lsJson << endl;
+//  std::cout << lsJson << endl;
 
-  // Parse the JSON and get the Results
-  //
-  Poco::Dynamic::Var loParsedJson = loParser.parse(lsJson);
-  Poco::Dynamic::Var loParsedJsonResult = loParser.result();
+  try
+  {
+    // Parse the JSON and get the Results
+    //
+    Poco::Dynamic::Var loParsedJson = loParser.parse(lsJson);
+    Poco::Dynamic::Var loParsedJsonResult = loParser.result();
 
-  int number = 7;
-  Poco::format("Log a number [%hu]", number);
+    int number = 7;
+    Poco::format("Log a number [%hu]", number);
   
-  // Get the JSON Object
-  //
-  Object::Ptr loJsonObject = loParsedJsonResult.extract<Object::Ptr>();
+    // Get the JSON Object
+    //
 
-  // Get the values for the member variables
-  //
-  //
-  cout << "TransactionCode             " << GetValue(loJsonObject, "TransactionCode")               << endl;
-  cout << "FileRecordSequenceNumber    " << GetValue(loJsonObject, "FileRecordSequenceNumber")      << endl;
-  cout << "TcrSequenceNumber           " << GetValue(loJsonObject, "TcrSequenceNumber")             << endl;
-  cout << "TransactionRouteIndicator   " << GetValue(loJsonObject, "TransactionRouteIndicator")     << endl;
-  cout << "MerchantEstablishmentNumber " << GetValue(loJsonObject, "MerchantEstablishmentNumber")   << endl;
-  cout << "MerchantName                " << GetValue(loJsonObject, "MerchantName")                  << endl;
-  cout << "MerchantCity                " << GetValue(loJsonObject, "MerchantCity")                  << endl;
+    Object::Ptr loJsonObject = loParsedJsonResult.extract<Object::Ptr>();
 
+    // Get the values for the member variables
+    //
+    //
+    cout << "TransactionCode             " << GetValue(loJsonObject, "TransactionCode")               << endl;
+    cout << "FileRecordSequenceNumber    " << GetValue(loJsonObject, "FileRecordSequenceNumber")      << endl;
+    cout << "TcrSequenceNumber           " << GetValue(loJsonObject, "TcrSequenceNumber")             << endl;
+    cout << "TransactionRouteIndicator   " << GetValue(loJsonObject, "TransactionRouteIndicator")     << endl;
+    cout << "MerchantEstablishmentNumber " << GetValue(loJsonObject, "MerchantEstablishmentNumber")   << endl;
+    cout << "MerchantName                " << GetValue(loJsonObject, "MerchantName")                  << endl;
+    cout << "MerchantCity                " << GetValue(loJsonObject, "MerchantCity")                  << endl;
+    
+  }
+  catch (Poco::Exception& exc)
+  {
+    std::cerr << exc.displayText() << std::endl;
+  }
+  
   return 0;
 }
 
